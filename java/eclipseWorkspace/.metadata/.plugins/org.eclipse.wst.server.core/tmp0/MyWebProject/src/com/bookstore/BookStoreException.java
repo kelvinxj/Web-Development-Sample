0d2dfe7693c8e6769af10deaec97e8f8d1a66bb8
xiejia <xiejia@cn.ibm.com>
2018-11-06 17:47:03 +0800

    unmanage java/ecclipseworkspace/.metadata folder

--- java/eclipseWorkspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/MyWebProject/src/com/bookstore/BookStoreException.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore;
-
-public class BookStoreException extends Exception {
-
-	public BookStoreException(Exception ex) {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- java/eclipseWorkspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/MyWebTomcat/WEB-INF/pages/header.jsp
+++
@@ -1,5 +0,0 @@
-<div>
-	<a href="${pageContext.request.contextPath}/book/">
-		<img src="${pageContext.request.contextPath}/resources/logo.png"/>
-	</a>
-</div>
