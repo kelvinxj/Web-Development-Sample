08b43037bd46307291957d3877082648097e590c
Xie Jia <mrkelvin@yeah.net>
2020-05-08 15:15:52 +0800

    check in log4jtest files

--- java/eclipseWorkspace/kelvinTestProject/src/com/kelvin/test/log4jTest/BasicTest.java
+++ java/eclipseWorkspace/kelvinTestProject/src/com/kelvin/test/log4jTest/BasicTest.java
@@ -14,35 +14,47 @@ import org.junit.Test;
 /*
  * run this porgram in console:
  * export project kelvinTestProject as jar file(no need to be runnable jar)
- * create a new folder, put the jar as following:
+ * create a folder named "Log4JTest", put the jar as following:
  * 
  * lib/kelvinTestProject.jar
  * lib/log4j-1.2.17.jar
  * Log4J.properties
  * 
- * cd to this folder, run command(Windows):
- * java -cp lib/* com.kelvin.test.log4jTest.BasicTest
+ * cd to "Log4JTest", run command(Windows):
+ * java -cp .;lib/* com.kelvin.test.log4jTest.BasicTest
  * 
- * you will see log message written to Customer.log
+ * you will see log message written to Customer.log of this folder.
  * 
- * for linux, first change file Log4J.properties to Log4j.properties, then run(do not forget the ":" after classpath):
+ * for linux, first change file Log4J.properties to log4j.properties, then run(do not forget the ":" after classpath):
+ * java -cp .:lib/*: com.kelvin.test.log4jTest.BasicTest
+ * or
  * java -cp lib/*: com.kelvin.test.log4jTest.BasicTest
  * 
  * pass parameter to log4j(change root logger):
- * java -Dhadoop.root.logger="INFO,file1" -cp lib/* com.kelvin.test.log4jTest.BasicTest
+ * java -Dhadoop.root.logger="INFO,file1" -cp .;lib/* com.kelvin.test.log4jTest.BasicTest
  * 
  * specify configuration file location:
  * java -Dlog4j.configuration=file:Log4J.properties.bak -cp lib/* com.kelvin.test.log4jTest.BasicTest
  * 
- * improvement: commands above only works when in current folder. if you are in another folder, still not work.
- * 
+ * running in another directory:
+ * java -Dlog4j.configuration=file:Log4JTest/Log4J.properties -cp Log4JTest/lib/* com.kelvin.test.log4jTest.BasicTest
+ * Note: the Customer.log should be in another directory, not in "Log4JTest" folder.
  */
 
 
 public class BasicTest {
 	public static void main(String[] args) throws Exception{
-		PropertyConfigurator.configure("Log4j.properties");
-		//if no log4j setting, will write log to console
+		//if comment "PropertyConfigurator.configure" and "BasicConfigurator.configure()"
+		//use default configuration file. if running in eclipse, log4j.properties is in src folder of this project.
+		//, if running in command line, you need to specify log4j.properties location in classpath:
+		//e.g.: java -cp .;lib/* com.kelvin.test.log4jTest.BasicTest //log4.properties in current folder, jar in ./lib folder
+		
+		//Log4j.properties should be in project root folder
+		//PropertyConfigurator.configure("Log4j.properties");
+
+		//using basic configuration, will write log to console
+		//BasicConfigurator.configure();
+		
 		Logger logger = Logger.getRootLogger();
 		logger.info("info. triggered from main()");
 		logger.warn("warning. triggered from main()");
@@ -54,7 +66,8 @@ public class BasicTest {
 
 	@Test
 	public void test() {
-		//if no log4j setting, will write log to console
+		//use default configuration file. if running in eclipse, log4j.properties is in src folder of this project.
+		//Customer.log is in project root folder
 		Logger logger = Logger.getRootLogger();
 		logger.info("Write info level message");
 		logger.warn("Write wanring level message");
@@ -66,6 +79,8 @@ public class BasicTest {
 	
 	@Test
 	public void testUsingPropertyFile(){
+		//Log4j.properties should be in project root folder
+		//Customer.log is in project root folder
 		PropertyConfigurator.configure("Log4j.properties");
 		//if no log4j setting, will write log to console
 		Logger logger = Logger.getRootLogger();
