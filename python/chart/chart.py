f30c2dee4c655ecb1b50bf63d10a91af791b3590
xiejia home PC <mrkelvin@yeah.net>
2020-05-10 16:00:32 +0800

    add python chart folder

---
+++ python/chart/chart.py
@@ -0,0 +1,32 @@
+import matplotlib
+import sys
+#matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+#plt.plot([1,2,3,4])
+#plt.ylabel('some numbers')
+#plt.show()
+begin=""
+end=""
+usingTimeRange = True
+if(len(sys.argv) == 1):
+    usingTimeRange = False
+else:
+    begin=sys.argv[1]
+    end=sys.argv[2]
+    usingTimeRange = True
+tempArr = []
+fileObj = open(r'C:\Users\xiejia-lihui\Desktop\nohup.out')
+for line in fileObj.readlines():
+    time = line.split(' ')[0]
+    temp = float(line.split(' ')[1])/1000
+    #print(temp)
+    if not(usingTimeRange):
+        tempArr.append(temp)
+    else:
+        if time >= begin and time <= end:
+            tempArr.append(temp)
+fileObj.close()
+plt.plot(tempArr)
+plt.ylabel('CPU temperature')
+plt.show()
+#plt.savefig('test.png')
