6b0e1fd37e86eed8b8353fe0495fb59c74a3b72a
Xie Jia <mrkelvin@yeah.net>
2020-05-14 23:37:42 +0800

    add mavanScalaApp project

---
+++ maven/mavenScalaApp/src/main/scala/com/kelvin/test/App.scala
@@ -0,0 +1,17 @@
+package com.kelvin.test
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
